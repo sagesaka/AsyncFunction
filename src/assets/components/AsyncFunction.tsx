a621e3da1116ff2b5d3ae2aa8b9fa216d6b212b7
sagesaka <sagesaka@riprice.co.jp>
2025-02-14 17:47:39 +0900

    promiseの理解

--- src/assets/components/AsyncFunction.tsx
+++ src/assets/components/AsyncFunction.tsx
@@ -1,5 +1,14 @@
 const AsyncFunction = () => {
-  
+
+// Promise の練習
+// 2秒後に'完了'を返すPromise
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('完了')
+  }, 2000)
+})
+
+
   return (
     <div>
 
